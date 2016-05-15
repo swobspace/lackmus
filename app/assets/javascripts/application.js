f7dc2ba251ab48fe0a9cb10411649ac66a8c202c
Wolfgang Barth <wob@swobspace.net>
2016-05-15 17:17:27 +0200

    bootstrap with rails template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,21 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+/// require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
+//= require jquery-ui/datepicker
+//= require jquery-ui/datepicker-de
+//= require datatables
+//= require datatables/media/js/dataTables.bootstrap
+//= require datatables-buttons
+//= require datatables-buttons/js/buttons.bootstrap
+//= require datatables-buttons/js/buttons.colVis
+//= require datatables-buttons/js/buttons.flash
+//= require datatables-buttons/js/buttons.html5
+//= require datatables-buttons/js/buttons.print
+//= require select2
+//= require select2/select2_locale_de
+/// require modernizr.touch.2.8.3
+//= require bootstrap
+//= require lackmus
