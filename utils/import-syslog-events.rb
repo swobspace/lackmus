7c884251bb7b57718c6d8c5a3050d01c0900643f
Wolfgang Barth <wob@swobspace.net>
2016-05-26 10:14:19 +0200

    script to import Syslog Events

---
+++ utils/import-syslog-events.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# -*- encoding : utf-8 -*-
+
+ENV['RAILS_ENV'] ||= 'development'
+require File.dirname(__FILE__) + '/../config/environment.rb'
+
+ImportSystemEventJob.perform_later()
+
