74d2c6253723672e6ee26946a0d0ff91d2889042
Wolfgang Barth <wob@swobspace.net>
2016-05-28 18:21:35 +0200

    complete model signature

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,2 +1,17 @@
 class Signature < ActiveRecord::Base
+  # -- associations
+  has_many :events, foreign_key: :alert_signature_id
+
+  # -- configuration
+  ACTIONS = ['drop', 'ignore', 'normal', 'alert']
+  # -- validations and callbacks
+  validates :signature_id, presence: true, uniqueness: true
+  validates :signature_info, presence: true
+  validates :action, :allow_blank => false,
+            :inclusion => {
+              in: Signature::ACTIONS,
+              message: "Select one of #{Signature::ACTIONS.join(", ")}"
+            }
+
+
 end
--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Signature, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:events) }
+
+  it { is_expected.to validate_presence_of(:signature_id) }
+  it { is_expected.to validate_presence_of(:signature_info) }
+
+  it { is_expected.to validate_uniqueness_of(:signature_id) }
+  it { is_expected.to validate_inclusion_of(:action).
+                        in_array(Signature::ACTIONS) }
+
+ it "should get plain factory working" do
+    f = FactoryGirl.create(:signature)
+    g = FactoryGirl.create(:signature)
+    expect(f).to be_valid
+    expect(g).to be_valid
+  end
+
 end
