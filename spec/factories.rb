fcd9d0d823dec8259703f16f66e812fed9994bf7
Wolfgang Barth <wob@swobspace.net>
2016-05-28 18:21:58 +0200

    factory for signature

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,12 +8,22 @@ FactoryGirl.define do
     "name_#{n}"
   end
 
+  sequence :number do |num|
+    num
+  end
+
   factory :event do
     event_time Time.now
     sensor "bordergate"
     src_ip "1.2.3.4"
     dst_ip "5.6.7.8"
     proto "TCP"
+  end
+
+  factory :signature do
+    signature_id { generate(:number) }
+    signature_info "brabbelfaselblub"
+    action "normal"
   end
 
   factory :systemevent, class: Syslog::Systemevent do
