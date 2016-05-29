8d17c429081adf4fb5b1902b4c74e039e8d9989b
Wolfgang Barth <wob@swobspace.net>
2016-05-29 15:42:51 +0200

    fix ascii-8 to utf-8 conversion problem

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def clean_whois(whois)
-    whois.to_s.each_line.reject{|x| x.strip =~ /^([#%*]|$)/}.join
+    whois.to_s.each_line.reject{|x| x.strip =~ /^([#%*]|$)/}.join.encode('UTF-8', {invalid: :replace, undef: :replace, replace: '.'})
   end
 end
