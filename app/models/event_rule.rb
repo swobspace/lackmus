3dc2fad232f182ec279d7435ce912eac4642f9d9
Wolfgang Barth <wob@swobspace.net>
2016-06-19 19:37:55 +0200

    more filter attributes

--- app/models/event_rule.rb
+++ app/models/event_rule.rb
@@ -3,10 +3,16 @@ class EventRule < ActiveRecord::Base
   # -- associations
   # -- configuration
   serialize :filter, Hash
-  ACTIONS = ['drop', 'ignore', 'investigate', 'alert']
+  ACTIONS = [
+    'drop', 'ignore', 'investigate', 'alert'
+  ]
 
-  FILTER_ATTRIBUTES = [ 'src_ip', 'src_port', 'dst_ip', 'dst_port',
-                        'signature_id' ]
+  FILTER_ATTRIBUTES = [ 
+    'sensor', 'src_ip', 'src_port', 'dst_ip', 'dst_port', 'proto',
+    'alert_signature_id', 'alert_category', 'alert_severity',
+    'http_hostname', 'http_xff', 'http_content_type', 'http_status',
+    'http_protocol', 'http_method'
+  ]
 
   acts_as_list
 
