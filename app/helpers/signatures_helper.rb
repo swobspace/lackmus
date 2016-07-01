6344e8fce8702a73ed6b1922f101a659e2450354
Wolfgang Barth <wob@swobspace.net>
2016-07-01 18:42:36 +0200

    add destroy_events_signature for usage in signature#index

--- app/helpers/signatures_helper.rb
+++ app/helpers/signatures_helper.rb
@@ -12,10 +12,11 @@ module SignaturesHelper
     if can? :edit, sig
       link += %Q[<li> #{edit_link(sig)}</li>]
     end
-    if can? :destroy, sig
-      link += %Q[<li>#{delete_link(sig)}</li>]
+    if can? :destroy, Event.new
+      link += %Q[<li>#{mark_events_done_link(sig)}</li>]
+      link += %Q[<li>#{delete_events_link(sig)}</li>]
     end
-    link += %Q[<li>#{link_to image_tag("wireshark.png"), pcap_signature_path(sig), 
+    link += %Q[<li>#{link_to image_tag("wireshark.png"), pcap_signature_path(sig),
                        class: 'btn btn-default', title: "Download pcap file" }</li>]
     link += %Q[  </ul>]
     link += %Q[ </div> ]
@@ -34,5 +35,21 @@ module SignaturesHelper
 
   def signatures_by_ip(ip)
     link_to "#{ip}", signatures_path(ip: ip)
+  end
+
+  def delete_events_link(sig)
+    link_to t('actions.destroy_all'),
+      destroy_events_signature_path(sig, commit: t('actions.destroy_all')),
+      method: :delete,
+      data: {confirm: "#{t('lackmus.really')} #{t('actions.destroy_all')}?\n\n#{t('lackmus.truly')}"},
+      class: "btn btn-danger"
+  end
+
+  def mark_events_done_link(sig)
+    link_to t('actions.all_done'),
+      destroy_events_signature_path(sig, {commit: t('actions.all_done')}),
+      method: :delete,
+      data: {confirm: "#{t('lackmus.really')} #{t('actions.all_done')}?"},
+      class: "btn btn-warning"
   end
 end
