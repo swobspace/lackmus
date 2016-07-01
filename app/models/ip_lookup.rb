49f7c62d16a60622d3eb82f705b3b1e64c34889e
Wolfgang Barth <wob@swobspace.net>
2016-07-01 18:34:58 +0200

    check if ip is member of a private subnet

--- app/models/ip_lookup.rb
+++ app/models/ip_lookup.rb
@@ -1,6 +1,9 @@
+require 'ipaddr'
+
 class IpLookup
   attr_reader :ip_addr
   def initialize(ip)
+    @ip_obj  = ip
     @ip_addr = ip.to_s
   end
 
@@ -12,9 +15,13 @@ class IpLookup
     self.new(ip).get_whois
   end
 
+  def self.is_private?(ip)
+    self.new(ip).check_private_subnet
+  end
+
   def get_hostname
     Rails.cache.fetch("ip/#{ip_addr}/hostname") do
-      begin 
+      begin
         Resolv.getname(ip_addr)
       rescue Resolv::ResolvError => e
         nil
@@ -31,6 +38,16 @@ class IpLookup
         nil
       end
     end
+  end
+
+  def check_private_subnet
+    @private_class_a ||= IPAddr.new('10.0.0.0/8')
+    @private_class_b ||= IPAddr.new('172.16.0.0/12')
+    @private_class_c ||= IPAddr.new('192.168.0.0/16')
+
+    @private_class_a.include?(@ip_obj) ||
+      @private_class_b.include?(@ip_obj) ||
+      @private_class_c.include?(@ip_obj)
   end
 
 end
--- spec/models/ip_lookup_spec.rb
+++ spec/models/ip_lookup_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe IpLookup, type: :model do
     it { expect(IpLookup.whois("example.com").to_s).to match /Registrar: RESERVED-INTERNET ASSIGNED NUMBERS AUTHORITY/ }
     it { expect(IpLookup.whois("192.0.2.1").to_s).to match /NetRange:       192.0.2.0 - 192.0.2.255/ }
   end
+
+  describe "#IpLookup.is_private(ip)" do
+    it { expect(IpLookup.is_private?("8.8.8.8")).to be_falsy }
+    it { expect(IpLookup.is_private?("192.168.1.2")).to be_truthy }
+    it { expect(IpLookup.is_private?("172.17.9.1")).to be_truthy }
+    it { expect(IpLookup.is_private?("10.1.2.4")).to be_truthy }
+  end
  
 end
