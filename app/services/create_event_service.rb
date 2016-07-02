509502139b91244b1693da2c3f35225f2bdbfc7b
Wolfgang Barth <wob@swobspace.net>
2016-07-02 14:28:52 +0200

    add event object to return struct

--- app/services/create_event_service.rb
+++ app/services/create_event_service.rb
@@ -1,5 +1,5 @@
 class CreateEventService
-  Result = ImmutableStruct.new( :success?, :error_messages )
+  Result = ImmutableStruct.new( :success?, :error_messages, :event )
 
   def initialize(options = {})
     @event_attributes = options
@@ -7,13 +7,13 @@ class CreateEventService
 
   def call
     if @event = Event.create(event_attributes) 
-      result = Result.new(success?: true, error_messages: [])
+      result = Result.new(success?: true, error_messages: [], event: event)
     else
       result = Result.new(success?: false, error_messages: @event.errors.messages)
     end
   end
 
 private
-  attr_reader :reader, :event_attributes
+  attr_reader :reader, :event_attributes, :event
   
 end
--- spec/services/create_event_service_spec.rb
+++ spec/services/create_event_service_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe CreateEventService do
     it { expect(subject.respond_to? :call).to be_truthy}
     it { expect(subject.call.respond_to? :success?).to be_truthy }
     it { expect(subject.call.respond_to? :error_messages).to be_truthy }
+    it { expect(subject.call.respond_to? :event).to be_truthy }
   end
 
   describe "#call" do
