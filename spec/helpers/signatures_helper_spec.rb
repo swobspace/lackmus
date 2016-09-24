59c24497a31fe556aacd8aab9985101813a65da8
Wolfgang Barth <wob@swobspace.net>
2016-09-24 13:45:44 +0200

    add host_report_link to signatures_by_ip

--- spec/helpers/signatures_helper_spec.rb
+++ spec/helpers/signatures_helper_spec.rb
@@ -6,18 +6,19 @@ RSpec.describe SignaturesHelper, type: :helper do
 
     context "with existing signature" do
       it "returns link_to signature" do
-	expect(signature_id_link("224466")).to eq("<a href=\"/signatures/#{signature.id}\">224466</a>")
+	expect(helper.signature_id_link("224466")).to eq("<a href=\"/signatures/#{signature.id}\">224466</a>")
       end
     end
     context "with nonexistent signature" do
       it "returns plain signature_id" do
-	expect(signature_id_link("1234999")).to eq("1234999")
+	expect(helper.signature_id_link("1234999")).to eq("1234999")
       end
     end
   end
   describe "#signatures_by_ip" do
     it "returns link to signatures_path with filter by ip" do
-      expect(signatures_by_ip("192.0.2.1")).to match(/signatures\?ip=192.0.2.1/)
+      expect(helper.signatures_by_ip("192.0.2.1")).to match(/signatures\?ip=192.0.2.1/)
+      puts helper.signatures_by_ip("192.0.2.1")
     end
   end
 end
