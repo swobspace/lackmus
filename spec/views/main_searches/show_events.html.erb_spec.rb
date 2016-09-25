26d9406f24d0cf9b6b1ff9f41b1686188e3130ef
Wolfgang Barth <wob@swobspace.net>
2016-09-25 11:45:00 +0200

    fix specs for main_searches#show templates

--- spec/views/main_searches/show_events.html.erb_spec.rb
+++ spec/views/main_searches/show_events.html.erb_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "main_searches/show.html.erb", type: :view do
+RSpec.describe "main_searches/show_events.html.erb", type: :view do
   before(:each) do
     @ability = Object.new
     @ability.extend(CanCan::Ability)
     allow(controller).to receive(:current_ability) { @ability }
     allow(controller).to receive(:controller_name) { "events" }
-    allow(controller).to receive(:action_name) { "index" }
+    allow(controller).to receive(:action_name) { "show" }
 
     assign(:events, FactoryGirl.create_list(:event, 3, src_ip: '192.0.2.8'))
   end
---
+++ spec/views/main_searches/show_signatures.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "main_searches/show_signatures.html.erb", type: :view do
+  before(:each) do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "events" }
+    allow(controller).to receive(:action_name) { "show" }
+
+    assign(:signatures, [
+      FactoryGirl.create(:signature, signature_id: 11224477),
+      FactoryGirl.create(:signature, signature_id: 33995467),
+      FactoryGirl.create(:signature, signature_id: 34251699),
+    ])
+  end
+
+  it "renders a list of events" do
+    render
+    assert_select "tr>td", :text => 11224477.to_s, :count => 1
+    assert_select "tr>td", :text => 33995467.to_s, :count => 1
+    assert_select "tr>td", :text => 34251699.to_s, :count => 1
+  end
+end
