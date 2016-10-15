92ead66a969d4e7c8658ba303a47df25093e7cfe
Wolfgang Barth <wob@swobspace.net>
2016-10-15 18:04:41 +0200

    implement host_reports#show view spec

--- spec/views/host_reports/show.html.erb_spec.rb
+++ spec/views/host_reports/show.html.erb_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "host_reports/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:showtime) { Time.now }
+  before(:each) do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(controller).to receive(:current_ability) { @ability }
+    allow(controller).to receive(:controller_name) { "host_reports" }
+    allow(controller).to receive(:action_name) { "show" }
+
+    assign(:signature, FactoryGirl.create(:signature, signature_id: 1234568))
+    @events = FactoryGirl.create_list(:event, 3, 
+      sensor: "sentinel",
+      event_time: showtime,
+      alert_signature_id: 1234568,
+      alert_signature: "Hastenichgesehn",
+      src_ip: "192.0.2.7",
+      dst_ip: "192.51.81.3",
+    )
+    assign(:host, "192.0.2.7")
+    assign(:is_form, true)
+  end
+
+  it "renders host_reports#show" do
+    render
+
+    expect(rendered).to match(/1234568/)
+    expect(rendered).to match(/sentinel/)
+    expect(rendered).to match(/Hastenichgesehn/)
+    expect(rendered).to match(/#{showtime.to_s(:precision)}/)
+    expect(rendered).to match(/192.0.2.7/)
+    expect(rendered).to match(/192.51.81.3/)
+  end
 end
