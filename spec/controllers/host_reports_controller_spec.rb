5f8f81d3836d0f9c853249f82b8ba39253db0c67
Wolfgang Barth <wob@swobspace.net>
2016-10-15 17:06:18 +0200

    fix mail delivery in specs

--- spec/controllers/host_reports_controller_spec.rb
+++ spec/controllers/host_reports_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe HostReportsController, type: :controller do
+  include ActiveJob::TestHelper 
+  include ActionMailer::TestHelper 
+
   login_admin
 
   let(:host) { '192.0.2.9' }
   let(:mail) { 'mailto@example.com' }
-  let(:events) { FactoryGirl.create_list(:event, 3, src_ip: host) }
+  let!(:events) { FactoryGirl.create_list(:event, 3, src_ip: host) }
 
   describe "GET #show" do
     it "assigns selected events as @events" do
@@ -23,10 +26,19 @@ RSpec.describe HostReportsController, type: :controller do
 
   describe "POST #create_mail" do
     context "with valid params" do
+      it "calls host_event_report" do
+        message_delivery = instance_double(ActionMailer::MessageDelivery)
+        expect(ReportMailer).to receive(:host_event_report).with(hash_including(ip: host, mail_to: mail, subject: "MySubjectForReport")).and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+        post :create_mail, params: {ip: host, mail_to: mail, subject: "MySubjectForReport"}
+      end
+
       it "sends a new mail" do
         expect { 
-          post :create_mail, params: {ip: host, mail_to: mail, subject: "MySubjectForReport"}
-         }.to change { ActionMailer::Base.deliveries.count }.by(1)
+          perform_enqueued_jobs do
+            post :create_mail, params: {ip: host, mail_to: mail, subject: "MySubjectForReport"}
+          end
+        }.to change { ActionMailer::Base.deliveries.count }.by(1)
       end
 
       it "redirects to host_reports#show" do
