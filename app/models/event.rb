2efbe5679538584acb0f5720c6c20109ccfd586d
Wolfgang Barth <wob@swobspace.net>
2018-07-13 12:30:59 +0200

    add optional: true to belongs_to association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   include EventConcerns
   # -- associations
-  belongs_to :signature, counter_cache: true,
+  belongs_to :signature, counter_cache: true, optional: true,
              foreign_key: :alert_signature_id,
              primary_key: :signature_id
-  belongs_to :event_rule
+  belongs_to :event_rule, optional: true
 
   # -- configuration
   alias_attribute :dest_ip, :dst_ip
