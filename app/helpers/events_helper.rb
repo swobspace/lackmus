cbbf48ea60a365f3f380409240358c9b8ba5b7a4
Wolfgang Barth <wob@swobspace.net>
2018-07-14 18:24:14 +0200

    more bootstrap4 styling

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,13 +4,15 @@ module EventsHelper
   end
 
   def events_by_ip(ip)
-    link_to("#{ip}", events_path(ip: ip)) + " " +
-    host_report_link(ip)
+    raw(%Q[<span class="mr-2">]+
+    link_to("#{ip}", events_path(ip: ip), class: 'mr-1') + " " +
+    host_report_link(ip) +
+    %Q[</span>])
   end
 
   def new_rule_from_event(event)
     button  = %Q[<button type="button" class="btn btn-secondary">]
-    button += %Q[<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>]
+    button += %Q[<span class="fas fa-fw fa-cog" aria-hidden="true"></span>]
     button += %Q[</button>]
 
     link_to button.html_safe, new_event_rule_path(event_id: event.to_param),
--- app/helpers/host_reports_helper.rb
+++ app/helpers/host_reports_helper.rb
@@ -1,6 +1,6 @@
 module HostReportsHelper
   def host_report_link(ip)
-    link_to "host_report", show_host_report_path(ip: ip), class: "btn btn-secondary btn-sm"
+    link_to("host_report", show_host_report_path(ip: ip), class: "btn btn-secondary btn-sm mr-1")
   end
 
 end
