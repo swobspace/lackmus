f1bae539bb4aa1d74cdfa27cc97acce0767c54aa
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2019-03-08 07:51:44 +0100

    update for lackmus

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   Rails.application.config.middleware.use(
     ExceptionNotification::Rack,
     email: {
-      email_prefix: "#{Beschlussdb.title_short}: ",
-      sender_address: %{"notifier" <#{Beschlussdb.mail_from}>},
+      email_prefix: "Lackmus: ",
+      sender_address: %{"notifier" <#{Lackmus.mail_from}>},
       exception_recipients: %w{Wolfgang.Barth@marienhaus.de},
       sections: %w{request backtrace}
     }
